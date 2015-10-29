c7e4db7ccea0be15cf3e9bb18b063d60fa7305d7
Jeroen Hooyberghs <jeroen@hooyberghs.org>
2015-10-29 16:22:34 +0100

    we need the storage param in the define too

--- manifests/director/storage.pp
+++ manifests/director/storage.pp
@@ -20,6 +20,7 @@
 define bacula::director::storage (
   $port          = '9103',
   $password      = 'secret',
+  $storage       = "${::fqdn}",
   $device_name   = "${::fqdn}-device",
   $media_type    = 'File',
   $maxconcurjobs = '1',
