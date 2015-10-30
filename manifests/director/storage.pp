3cadd8cfea26ad782d53267822db2c4d2fe48a13
Jeroen Hooyberghs <jeroen@hooyberghs.org>
2015-10-30 14:44:20 +0100

    Problem with name inconsitency

--- manifests/director/storage.pp
+++ manifests/director/storage.pp
@@ -21,6 +21,7 @@ define bacula::director::storage (
   $port          = '9103',
   $password      = 'secret',
   $storage       = "${::fqdn}",
+  $storage_name  = "${::fqdn}",
   $device_name   = "${::fqdn}-device",
   $media_type    = 'File',
   $maxconcurjobs = '1',
